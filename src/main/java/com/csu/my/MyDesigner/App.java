c4bc83a9442ddde2caf5ca41e5d88f42daa6266a
liuxm <zhongnandaxuelxm@126.com>
2019-10-31 08:27:39 +0800

    1、首次提交

---
+++ src/main/java/com/csu/my/MyDesigner/App.java
@@ -0,0 +1,13 @@
+package com.csu.my.MyDesigner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
